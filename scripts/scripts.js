d453816e5b81e487d47bcedbdced8e92ec25dfcd
Jobayer Hosen <jobayerh99@gmail.com>
2024-05-04 22:14:22 +0600

    trying hard but not finding solutions till morning

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,18 @@
-function ticket(){
+function ticket() {
     hideElementById('home-section');
     hideElementById('offer-section');
     showElementById('ticket-section');
+}
+
+function scrollDown(x, y) {
+    window.scrollBy(x, y);
+}
+
+function seatClicked() {
+    document.getElementById("A1").onclick = null;
+    let el = document.getElementById('A1');
+    const value = (el.ariaLabel);
+    console.log(value);
+    setColor();
+    removeColor();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,14 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+// function setColor(){
+//     const color = document.getElementById('A1');
+//     color.addEventListener('click', function() {
+//         if(color.checked){
+//             color.classList.add('bg-[#1DD100]');
+//         }else{
+//             color.classList.remove('bg-[#1DD100]');
+//         }
+//     })
+// }
